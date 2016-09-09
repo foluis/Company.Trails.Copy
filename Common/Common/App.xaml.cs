ef1ec8170fd1e03c2d26c3995074c7826a1568a3
Luis Forero <foluis@hotmail.com>
2016-09-09 16:10:01 -0500

    Add project files.

---
+++ Common/Common/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Common.Pages;
+
+using Xamarin.Forms;
+
+namespace Common
+{
+    public partial class App : Application
+    {
+        public static bool IsUserLoggedIn { get; set; }
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new Login();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Common/Common/Pages/Login.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Common.Pages
+{
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        //async void OnLoginButtonClicked(object sender, EventArgs e)
+        //{
+        //    var isValid = AreCredentialsCorrect();
+        //    if (isValid)
+        //    {
+        //        App.IsUserLoggedIn = true;
+        //        Navigation.InsertPageBefore(new MainMenu(), this);
+        //        await Navigation.PopAsync();
+        //    }
+        //    else
+        //    {
+        //        //messageLabel.Text = "Login failed";            
+        //    }
+        //}
+
+        //void OnLoginButtonClicked(object sender, EventArgs e)
+        //{
+        //    var isValid = AreCredentialsCorrect();
+        //    if (isValid)
+        //    {
+        //        App.IsUserLoggedIn = true;
+        //        Navigation.PushModalAsync(new MainMenu());
+        //        Navigation.PopAsync();
+        //    }
+        //    else
+        //    {
+        //        //messageLabel.Text = "Login failed";            
+        //    }
+        //}
+        
+
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            string sera = "hola";
+        }
+
+        bool AreCredentialsCorrect()
+        {
+            return true;
+        }
+    }
+}
